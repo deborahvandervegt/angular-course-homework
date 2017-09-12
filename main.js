52befd4fbd7d7793b1b152a9ed2f7ad5cbd003d3
Deborah van der Vegt <deborah.vander.vegt@sogeti.com>
2017-09-12 16:06:15 +0200

    Add a list of ingredients

--- main.js
+++ main.js
@@ -7,20 +7,23 @@
 
     function ingredientController() {
         var vm = this;
+        var ingredients = [];
 
         vm.name = '';
         vm.ingredientName = '';
 
         vm.addIngredientToList = function () {
             vm.name = vm.ingredientName;
-            
-            var ingredients = [];
-            
-            for(var i = 0; i < vm.name.length; i++) {
-                ingredients += vm.name[i];
+
+            ingredients[ingredients.length++] = vm.name;
+        }
+
+        vm.getIngredientList = function () {
+            if (ingredients.length > 0) {
+                return ingredients;
             }
-            console.log(ingredients);
-            return ingredients;
+
+            return '';
         }
 
     }
